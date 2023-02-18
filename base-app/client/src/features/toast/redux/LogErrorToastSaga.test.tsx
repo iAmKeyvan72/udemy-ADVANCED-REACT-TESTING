7d9d402077999a91cefc5226d720d27675d77bc8
Keyvan Mahmoudi <5063636+iAmKeyvan72@users.noreply.github.com>
2023-02-18 06:07:58 +0100

    Finish section 2

---
+++ base-app/client/src/features/toast/redux/LogErrorToastSaga.test.tsx
@@ -0,0 +1,37 @@
+import { PayloadAction } from '@reduxjs/toolkit';
+import { expectSaga } from 'redux-saga-test-plan';
+
+import { ToastOptions } from '../types';
+import { logErrorToast, logErrorToasts } from './LogErrorToastSaga';
+
+const errorToastOptions: ToastOptions = {
+  title: 'Showwww fooking error',
+  status: 'error',
+};
+
+const errorToastAction: PayloadAction<ToastOptions> = {
+  type: 'test',
+  payload: errorToastOptions,
+};
+
+test('saga calls analytics when faces error', () => {
+  return expectSaga(logErrorToasts, errorToastAction)
+    .call(logErrorToast, 'Showwww fooking error')
+    .run();
+});
+
+const notErrorToastOptions: ToastOptions = {
+  title: "This won't be a fooookin error",
+  status: 'warning',
+};
+
+const notErrorToastOptionsAction: PayloadAction<ToastOptions> = {
+  type: 'warningTest',
+  payload: notErrorToastOptions,
+};
+
+test('do not send analytics data when there is no foookin error', () => {
+  return expectSaga(logErrorToasts, notErrorToastOptionsAction)
+    .not.call.fn(logErrorToast)
+    .run();
+});
